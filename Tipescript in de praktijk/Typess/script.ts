d6ce9fe590b9e8a8c226c4dfd9b9812a02ee46d9
René Junior Van Asch <rene.jr.van.asch@gmail.com>
2023-09-29 14:05:45 +0200

    Seeing all the types
    seeing all the types that can be used in typescript.

---
+++ Tipescript in de praktijk/Typess/script.ts
@@ -0,0 +1,33 @@
+
+const naam: string = 'Rene'
+
+type naam1 = string
+
+const waarheid: boolean = true
+
+const nummer: number = 1
+
+const arrayNummers: number[] = [1,2,3,4,5,6,7]
+
+const naam2: [string, number, boolean] = ['rene', 3, false] // tuple
+
+type persoon = {
+    naam: string,
+    leeftijd: 26,
+    stad: string
+}
+const persoon= {
+    naam: 'rene',
+    leeftijd: 26,
+    stad: 'Antwerpen'
+}
+
+enum kleuren {
+    WHITE = '#FFF',
+    BLACK = '#000',
+    GRAY = '#F2F2F2'
+}
+
+kleuren.GRAY
+
+
